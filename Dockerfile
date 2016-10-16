2bc930f11ef3c3f55c9b50e17f7e6b1486392535
pb <pb@pb.local>
2016-10-16 13:44:37 -0700

    new one

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM java:7
-COPY JavaHelloWorld .
-RUN javac JavaHelloWorld
-CMD ["java","JavaHelloWorld"]
+COPY JavaHelloWorld.java .
+RUN javac JavaHelloWorld.java
 
+CMD ["java", "JavaHelloWorld"]
---
+++ JavaHelloWorld.java
@@ -0,0 +1,6 @@
+public class JavaHelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, JavaWorld");
+   }
+}
