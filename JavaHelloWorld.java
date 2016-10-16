234a0fdfdea1b5edb375848d0d287e9c4e111c3c
Coder63 <pbharath63@gmail.com>
2016-10-16 13:30:25 -0700

    fixed it

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -1,9 +1,6 @@
-public class JavaHelloWorld
-{
-	public static void main(String [] args)
-	{
-		System.out.println("Java Hello World");
-		System.out.println("Hello world");
-	}
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
 }
-
