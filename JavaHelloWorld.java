d2c0e7ed034788e01b9d11b50a29c84fb5ba099c
Coder63 <pbharath63@gmail.com>
2017-06-13 14:23:24 -0700

    Delete JavaHelloWorld.java

--- JavaHelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class JavaHelloWorld {
-   public static void main(String[] args) {
-      //  This is the only line I can edit 
-	// Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, JavaWorld");
-   }
-}
